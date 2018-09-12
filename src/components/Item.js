d1b0f862fe51ffc723e4e18deeab29e8f71484fd
Hani <msowe.feha@gmail.com>
2018-09-13 01:39:36 +0300

    CSS updates

--- src/components/Item.js
+++ src/components/Item.js
@@ -10,51 +10,57 @@ class Item extends Component {
 
         {items.map((item)=>
 
-        <div className="flex-container perfect-centering item-border-3px">
-          <div className="perfect-centering">
-            <ImageParallax image={item.image} title={item.title}/>
-          </div>
-          <div className="flex-container-col item-details border-line-3px ">
-            <div className="font-title">{item.title}</div>
-            <div className="font-description">{item.description}</div>
-            <div className="font-price">{item.price}</div>
-          </div>
-          <button className="btn btn-lg btn-add-edit perfect-centering font-description">add/edit</button>
-        </div>
+          <div className="flex-container perfect-centering item-border-3px">
+            <div className="perfect-centering">
+              <img src={item.image} className="item-image" alt={item.title}/>
 
-      )}
+              {/* <ImageParallax image={item.image} title={item.title}/> */}
+            </div>
+            <div className="flex-container-col item-details border-line-3px ">
+              <h2 class="item-title">{item.title}</h2>
+              <div className="font-description">{item.description}</div>
+              <div className="font-price">{item.price} S.R.</div>
+            </div>
+
+            <div className="perfect-centering">
+              <button className="btn btn-lg btn-add-edit perfect-centering font-description">add/edit</button>
+            </div>
+
+          </div>
+
+        )}
 
         {/* {items.map((item)=>
           <div className="mtam" key={item.id}>
           <div className="flex-container">
-            <div className="perfect-centering">
-              <div className="mtam-icon">
-                <div className="example-mtam-icon">
-                  <ImageParallax image={item.image} title={item.title} />
-                </div>
-              </div>
-            </div>
-            <div className="flex-container-col item-details">
-              <div className="mtam-title padding-top-3">{item.title}</div>
-              <div className="mtam-subtitle text-gray ptb-5">{item.description}</div>
-              <span className="">
-                {item.price} S.R.
-              </span>
-            </div>
-            <div className="perfect-centering">
-              <div className="">
-                <span className="">
-                  ADD/EDIT
-                </span>
-              </div>
-            </div>
-          </div>
-
-          </div>
-        )}
+          <div className="perfect-centering">
+          <div className="mtam-icon">
+          <div className="example-mtam-icon">
+          <ImageParallax image={item.image} title={item.title} />
+        </div>
       </div>
-    )
-  }
+    </div>
+    <div className="flex-container-col item-details">
+    <div className="mtam-title padding-top-3">{item.title}</div>
+    <div className="mtam-subtitle text-gray ptb-5">{item.description}</div>
+    <span className="">
+    {item.price} S.R.
+  </span>
+</div>
+<div className="perfect-centering">
+<div className="">
+<span className="">
+ADD/EDIT
+</span>
+</div>
+</div>
+</div>
+
+</div>
+)} */}
+</div>
+)
+}
 }
 
 export default Item;
